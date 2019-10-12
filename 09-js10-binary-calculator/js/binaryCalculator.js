a91ba9c1f465c23f2c82aee9bcaf8690f9c6ef16
Wojciech Diechtiar <wojciech.diechtiar@gmail.com>
2019-10-12 20:00:34 +0200

    Day 9 of #365DaysOfCode

---
+++ 09-js10-binary-calculator/js/binaryCalculator.js
@@ -0,0 +1,35 @@
+//create functions for all arithmetic operations
+
+
+function binaryAdd(operand1, operand2) {
+  
+}
+function binarySub(operand1, operand2) {
+  
+}
+function binaryMul(operand1, operand2) {
+  
+}
+function binaryDiv(operand1, operand2) {
+  
+}
+
+//convert string to binary number
+
+function toBinary(string) {
+
+  return binaryNumber;
+}
+
+//convert string to decimal number
+
+function toDecimal(string) {
+
+  return decimalNumber;
+}
+
+//parse input and perform calculations
+
+//read input
+
+//update and display result

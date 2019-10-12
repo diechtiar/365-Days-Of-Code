1f031f2b87537e4b5a615a51243b52321c8b31b9
Wojciech Diechtiar <wojciech.diechtiar@gmail.com>
2019-10-12 21:25:20 +0200

    Day 9 of #365DaysOfCode
    Working on it

--- 09-js10-binary-calculator/js/binaryCalculator.js
+++ 09-js10-binary-calculator/js/binaryCalculator.js
@@ -1,17 +1,17 @@
 //create functions for all arithmetic operations
 
-
 function binaryAdd(operand1, operand2) {
-  
+  return parseInt(operand1, 2) + parseInt(operand2, 2);
 }
+
 function binarySub(operand1, operand2) {
-  
+  return parseInt(operand1, 2) - parseInt(operand2, 2);
 }
 function binaryMul(operand1, operand2) {
-  
+  return parseInt(operand1, 2) * parseInt(operand2, 2);
 }
 function binaryDiv(operand1, operand2) {
-  
+  return parseInt(operand1, 2) / parseInt(operand2, 2);
 }
 
 //convert string to binary number
@@ -33,3 +33,11 @@ function toDecimal(string) {
 //read input
 
 //update and display result
+
+  
+  
+  document.addEventListener('DOMContentLoaded', (event) => {
+    console.log((binaryDiv('1101', '110') >>> 0).toString(2));
+    document.getElementById('res').innerHTML = (binaryDiv('1101', '110') >>> 0).toString(2);
+  });
+  

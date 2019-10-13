5fc6a3fc0664533474f08d5bf4711bc1322bd9a5
Wojciech Diechtiar <wojciech.diechtiar@gmail.com>
2019-10-13 20:54:22 +0200

    Day 10 of #365DaysOfCode
    Finished binary calculator

--- 09-js10-binary-calculator/js/binaryCalculator.js
+++ 09-js10-binary-calculator/js/binaryCalculator.js
@@ -1,43 +1,110 @@
 //create functions for all arithmetic operations
 
 function binaryAdd(operand1, operand2) {
-  return parseInt(operand1, 2) + parseInt(operand2, 2);
+  result = (parseInt(operand1, 2) + parseInt(operand2, 2)).toString(2);
+  console.log(result);
+  return result;
 }
 
 function binarySub(operand1, operand2) {
-  return parseInt(operand1, 2) - parseInt(operand2, 2);
+  result = (parseInt(operand1, 2) - parseInt(operand2, 2)).toString(2);
+  console.log(result);
+  return result;
 }
+
 function binaryMul(operand1, operand2) {
-  return parseInt(operand1, 2) * parseInt(operand2, 2);
+  result = (parseInt(operand1, 2) * parseInt(operand2, 2)).toString(2);
+  console.log(result);
+  return result;
 }
+
 function binaryDiv(operand1, operand2) {
-  return parseInt(operand1, 2) / parseInt(operand2, 2);
+  result = (parseInt(operand1, 2) / parseInt(operand2, 2)).toString(2);
+  console.log(result);
+  return result;
 }
 
-//convert string to binary number
+//perform calculations and return result 
 
-function toBinary(string) {
+function calculate(param) {
 
-  return binaryNumber;
+//  let result = (binaryDiv('1101', '110') >>> 0).toString(2);
+  let operand1 = '';
+  let operand2 = '';
+  let operator = '';
+  let result = '';
+
+  if (param.indexOf('+') !== -1) {
+    operator = '+';
+  } else if (param.indexOf('-') !== -1) {
+    operator = '-';
+  } else if (param.indexOf('*') !== -1) {
+    operator = '*';
+  } else if (param.indexOf('/') !== -1) {
+    operator = '/';
+  } 
+  
+  operand1 = param.substring(0, param.indexOf(operator));
+  operand2 = param.substring((param.indexOf(operator) + 1));
+
+  switch(operator) {
+    case '+': 
+      result = binaryAdd(operand1, operand2);
+      break;
+    case '-': 
+      result = binarySub(operand1, operand2);
+      break;
+    case '*': 
+      result = binaryMul(operand1, operand2);
+      break;
+    case '/': 
+      result = binaryDiv(operand1, operand2);
+      break;
+  }
+  console.log(result);
+  return result;
 }
-
-//convert string to decimal number
-
-function toDecimal(string) {
-
-  return decimalNumber;
-}
-
-//parse input and perform calculations
-
-//read input
 
 //update and display result
+document.addEventListener('DOMContentLoaded', (event) => {
 
-  
-  
-  document.addEventListener('DOMContentLoaded', (event) => {
-    console.log((binaryDiv('1101', '110') >>> 0).toString(2));
-    document.getElementById('res').innerHTML = (binaryDiv('1101', '110') >>> 0).toString(2);
+  let btn0 = document.getElementById('btn0');
+  let btn1 = document.getElementById('btn1');
+  let resultDisplay = document.getElementById('res');
+  let eqlButton = document.getElementById('btnEql');
+  let clrButton = document.getElementById('btnClr');
+  let sumButton = document.getElementById('btnSum');
+  let subButton = document.getElementById('btnSub');
+  let mulButton = document.getElementById('btnMul');
+  let divButton = document.getElementById('btnDiv');
+ 
+  btn0.addEventListener('click', function(event) {
+    resultDisplay.innerHTML += '0';
   });
+  btn1.addEventListener('click', function(event) {
+    resultDisplay.innerHTML += '1';
+  });
+
+  sumButton.addEventListener('click', function(event) {
+    resultDisplay.innerHTML += '+';
+  });
+  subButton.addEventListener('click', function(event) {
+    resultDisplay.innerHTML += '-';
+  });
+  mulButton.addEventListener('click', function(event) {
+    resultDisplay.innerHTML += '*';
+  });
+  divButton.addEventListener('click', function(event) {
+    resultDisplay.innerHTML += '/';
+  });
+  
+  eqlButton.addEventListener('click', function(event) {
+    resultDisplay.innerHTML = calculate(resultDisplay.innerHTML);
+  });
+  
+  clrButton.addEventListener('click', function (event) {
+    resultDisplay.innerHTML = '';
+  });
+
+});
   

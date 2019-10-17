0797971c024f1d45d2147e05aa44249a1d058f75
Wojciech Diechtiar <wojciech.diechtiar@gmail.com>
2019-10-17 23:59:21 +0200

    Inheritance problem solved

--- 30-inheritance.js
+++ 30-inheritance.js
@@ -1,20 +1,33 @@
+'use strict';
+
+var _input = '';
+var _index = 0;
+process.stdin.on('data', (data) => { _input += data; });
+process.stdin.on('end', () => {
+    _input = _input.split(new RegExp('[ \n]+'));
+    main();    
+});
+function read() { return _input[_index++]; }
+
+/**** Ignore above this line. ****/
+
 class Person {
-  constructor(firstName, lastName, identification) {
-      this.firstName = firstName;
-      this.lastName = lastName;
-      this.idNumber = identification;
-  }
-  
-  printPerson() {
-      console.log(
-          "Name: " + this.lastName + ", " + this.firstName 
-          + "\nID: " + this.idNumber
-      )
-  }
+    constructor(firstName, lastName, identification) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.idNumber = identification;
+    }
+    
+    printPerson() {
+        console.log(
+            "Name: " + this.lastName + ", " + this.firstName 
+            + "\nID: " + this.idNumber
+        )
+    }
 }
 
 class Student extends Person {
-  /*	
+  /*    
   *   Class Constructor
   *   
   *   @param firstName - A string denoting the Person's first name.
@@ -27,10 +40,10 @@ class Student extends Person {
     super()
     this.firstName = firstName
     this.lastName = lastName
-    this.id = id
+    this.idNumber = id
     this.scores = scores
   }
-  /*	
+  /*    
   *   Method Name: calculate
   *   @return A character denoting the grade.
   */
@@ -43,24 +56,36 @@ class Student extends Person {
   * (55 <= a) && (a <= 70) => 'P'
   * (40 <= a) && (a <= 55) => 'D'
   * (a < 40) => 'T'
-  
-    return grade
+  */
+    let divideBy = this.scores.length;
+
+    let grade = this.scores.reduce((prev, curr) => {
+        return (prev + curr)
+    })
+    grade = grade / divideBy
+
+    if ((90 <= grade) && (grade <= 100)) return 'O'
+    if ((80 <= grade) && (grade <= 90)) return 'E'
+    if ((70 <= grade) && (grade <= 80)) return 'A'
+    if ((55 <= grade) && (grade <= 70)) return 'P'
+    if ((40 <= grade) && (grade <= 55)) return 'D'
+    if ((grade < 40)) return 'T'
   }
 }
 
 function main() {
-  let firstName = read()
-  let lastName = read()
-  let id = +read()
-  let numScores = +read()
-  let testScores = new Array(numScores)
-  
-  for (var i = 0; i < numScores; i++) {
-      testScores[i] = +read()  
-  }
+    let firstName = read()
+    let lastName = read()
+    let id = +read()
+    let numScores = +read()
+    let testScores = new Array(numScores)
+    
+    for (var i = 0; i < numScores; i++) {
+        testScores[i] = +read()  
+    }
 
-  let s = new Student(firstName, lastName, id, testScores)
-  s.printPerson()
-  s.calculate()
-  console.log('Grade: ' + s.calculate())
+    let s = new Student(firstName, lastName, id, testScores)
+    s.printPerson()
+    s.calculate()
+    console.log('Grade: ' + s.calculate())
 }

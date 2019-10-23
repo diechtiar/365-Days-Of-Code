0039b2d8c7132644bf0a307c41352c1902b80ef0
Wojciech Diechtiar <wojciech.diechtiar@gmail.com>
2019-10-23 19:00:11 +0200

    Create convert-html-entities.js
    Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.

---
+++ convert-html-entities.js
@@ -0,0 +1,30 @@
+/*
+ * Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.
+ *
+ */
+
+function convertHTML(str) {
+  // &colon;&rpar;
+  let htmlEntities = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '\"': '&quot;',
+    '\'': '&apos;'
+  };
+
+  let result = '';
+  
+  for (let [char, entity] of Object.entries(htmlEntities)) {
+    let regex = new RegExp(char, 'gi');
+    str = str.replace(regex, entity);
+  }
+  
+  return str;
+}
+
+console.log(convertHTML("Dolce & Gabbana"));
+console.log(convertHTML("Hamburgers < Pizza < Tacos"));
+console.log(convertHTML("<>"));
+
+

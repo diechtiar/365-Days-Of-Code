9a287b6216b3e540eae85e686e64b154a6350638
Wojciech Diechtiar <wojciech.diechtiar@gmail.com>
2020-01-29 19:58:56 +0100

    Adds controlled form

--- react/create-a-controlled-form.js
+++ react/create-a-controlled-form.js
@@ -1,3 +1,13 @@
+/*
+The MyForm component is set up with an empty form with a submit handler. The submit handler will be called when the form is submitted.
+
+We've added a button which submits the form. You can see it has the type set to submit indicating it is the button controlling the form. Add the input element in the form and set its value and onChange() attributes like the last challenge. You should then complete the handleSubmit method so that it sets the component state property submit to the current input value in the local state.
+
+Note:  You also must call event.preventDefault() in the submit handler, to prevent the default form submit behavior which will refresh the web page.
+
+Finally, create an h1 tag after the form which renders the submit value from the component's state. You can then type in the form and click the button (or press enter), and you should see your input rendered to the page.
+*/
+
 class MyForm extends React.Component {
     constructor(props) {
         super(props);
@@ -15,7 +25,10 @@ class MyForm extends React.Component {
     }
     handleSubmit(event) {
         // change code below this line
-
+        this.setState({
+            submit: this.state.input
+        });
+        event.preventDefault();
         // change code above this line
     }
     render() {
@@ -23,12 +36,12 @@ class MyForm extends React.Component {
             <div>
                 <form onSubmit={this.handleSubmit}>
                     { /* change code below this line */}
-
+                    <input value={this.state.input} onChange={this.handleChange}></input>
                     { /* change code above this line */}
-                    <button type='submit'>Submit!</button>
+                    <button type='submit' onSubmit={this.handleSubmit}>Submit!</button>
                 </form>
                 { /* change code below this line */}
-
+                <h1>{this.state.submit}</h1>
                 { /* change code above this line */}
             </div>
         );
